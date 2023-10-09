df1e279e2a55d9157ce6ae06309b056832e057c8
Deep Bansode <drasticoder@gmail.com>
2023-10-09 21:28:55 +0530

    feat: sign jwt token &resettoken

--- mongooseModel.js
+++ mongooseModel.js
@@ -1,6 +1,8 @@
 import mongoose, { Schema } from "mongoose";
 import validator from "validator";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
 const schema = new mongoose.Schema(
   {
@@ -18,7 +20,14 @@ const schema = new mongoose.Schema(
       required: true,
       select: false,
       minlength: 8,
-    }, 
+    },
+    refreshToken: [
+      {
+        type: String,
+      },
+    ],      
+    resetPasswordToken: String,
+    resetPasswordExpire: String,
     deleted: {
       type: Boolean,
       default: false,
@@ -36,6 +45,24 @@ schema.pre("save", async function (next) {
 
 schema.methods.comparePassword = async function (password) {
   return await bcrypt.compare(password, this.password);
+};
+
+schema.methods.getJWTToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+
+schema.methods.getResetToken = async function () {
+  const resetToken = crypto.randomBytes(20).toString("hex");
+
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  this.resetPasswordExpire = Date.now() + 50 * 60 * 1000;
+  return resetToken;
 };
 
 export const Model = mongoose.model("model", schema);
